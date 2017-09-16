3b16ecea9abc7153a5a2371c168f8b907b5fa9e1
Max Peiros <mpeiros@gmail.com>
2017-09-15 18:36:01 -0700

    Update App component to display html and summary after receiving response from Rails API

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    html: '',
+    summary: {}
+  };
+
+  componentDidMount() {
+    axios.get('http://localhost:3000?url=http://www.google.com')
+      .then(response => this.setState({ html: response.data.html, summary: response.data.summary }));
+  }
+
+  renderSummary() {
+    return Object.keys(this.state.summary).map(key => {
+      return <p key={key}>{key}: {this.state.summary[key]}</p>;
+    });
+  }
+
   render() {
     return (
       <div>
-        Hello World!
+        <pre>
+          {this.state.html}
+        </pre>
+        {this.renderSummary()}
       </div>
     );
   }
